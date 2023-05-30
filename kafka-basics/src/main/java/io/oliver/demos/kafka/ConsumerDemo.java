f9573fa95d57e0a3244858a52f882a9f664d7ace
orestesoliveira <orestesoliveira@gmail.com>
2023-05-30 09:20:43 -0300

    Fixed imports

--- kafka-basics/src/main/java/io/oliver/demos/kafka/ConsumerDemo.java
+++ kafka-basics/src/main/java/io/oliver/demos/kafka/ConsumerDemo.java
@@ -1,9 +1,6 @@
 package io.oliver.demos.kafka;
 
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
